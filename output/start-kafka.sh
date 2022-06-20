e926f57818d517b3814a77e5a361d3e21bb514f2
Vicente González <v.gonzalez@peza.io>
2022-06-20 20:41:28 +0200

    feat: update kinesis configuration

--- output/start-kafka.sh
+++ output/start-kafka.sh
@@ -2,3 +2,4 @@
 
 # connector start command here.
 exec "/opt/kafka/bin/connect-standalone.sh" "/opt/kafka/config/connect-standalone.properties"  "/opt/kafka/config/connect-kinesis-sink.properties"
+# exec "/opt/kafka/bin/connect-distributed.sh" "/opt/kafka/config/connect-distributed.properties"
