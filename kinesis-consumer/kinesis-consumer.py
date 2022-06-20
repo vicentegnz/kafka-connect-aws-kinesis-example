e0ca03eb051f19b167a637136467b1aee852819a
Vicente González <v.gonzalez@peza.io>
2022-06-20 15:27:40 +0200

    feat: kinesis connector working

--- kinesis-consumer/kinesis-consumer.py
+++ kinesis-consumer/kinesis-consumer.py
@@ -1,20 +1,17 @@
 import boto3
 import json
 import time
-import os
 
 print('Starting..')
 session = boto3.Session(region_name='eu-west-1')
 client = session.client(
     'kinesis',
-    aws_access_key_id='',
-    aws_secret_access_key='',
-    endpoint_url='http://host.docker.internal:4567'
+    aws_access_key_id='FakeKey',
+    aws_secret_access_key='FakeKey',
+    endpoint_url='http://kinesis:4566'
 )
 
 list_streams = client.list_streams()
-print(list_streams)
-
 if 'simple-connect-stream' not in list_streams['StreamNames']:
     client.create_stream(StreamName='simple-connect-stream', ShardCount=1)
     time.sleep(1)
@@ -25,7 +22,7 @@ shard_id = stream_details['StreamDescription']['Shards'][0]['ShardId']
 response = client.get_shard_iterator(
     StreamName='simple-connect-stream',
     ShardId=shard_id,
-    ShardIteratorType='TRIM_HORIZON'
+    ShardIteratorType='LATEST'
 )
 
 shard_iterator = response['ShardIterator']
@@ -36,7 +33,6 @@ while True:
     shard_iterator = response['NextShardIterator']
     for record in response['Records']:
         if 'Data' in record and len(record['Data']) > 0:
+            print('\nEvent received from KAFKA:')
             print(json.loads(record['Data']))
     time.sleep(0.75)
-
-print("Consumer stopped.")
